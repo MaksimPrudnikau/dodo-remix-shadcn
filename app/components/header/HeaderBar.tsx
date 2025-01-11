96975fa89a2fb2bb0954fbd29e37e2607a7f9f66
Maksim Prudnikau <60088883+MaksimPrudnikau@users.noreply.github.com>
2025-01-11 12:48:08 +0300

    add pulse animation

--- app/components/header/HeaderBar.tsx
+++ app/components/header/HeaderBar.tsx
@@ -4,7 +4,7 @@ import { Link } from "@remix-run/react";
 export default function HeaderBar() {
   return (
     <div className={"flex gap-5"}>
-      <Link to={'/stream'}>Прямой эфир</Link>
+      <Link id={'stream'} to={'/stream'}>Прямой эфир</Link>
       <Link to={'/about'}>О нас</Link>
       <Link to={'/contacts'}>Контакты</Link>
     </div>
