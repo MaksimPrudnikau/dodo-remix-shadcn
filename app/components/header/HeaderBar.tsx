791a98ea9f5be482eb2b3c0355195909a2fef1b5
Maksim Prudnikau <60088883+MaksimPrudnikau@users.noreply.github.com>
2025-01-11 13:00:52 +0300

    prettier format

--- app/components/header/HeaderBar.tsx
+++ app/components/header/HeaderBar.tsx
@@ -1,13 +1,14 @@
-import './styles.css'
+import "./styles.css";
 import { Link } from "@remix-run/react";
 
 export default function HeaderBar() {
   return (
     <div className={"flex gap-5"}>
-      <Link id={'stream'} to={'/stream'}>Прямой эфир</Link>
-      <Link to={'/about'}>О нас</Link>
-      <Link to={'/contacts'}>Контакты</Link>
+      <Link id={"stream"} to={"/stream"}>
+        Прямой эфир
+      </Link>
+      <Link to={"/about"}>О нас</Link>
+      <Link to={"/contacts"}>Контакты</Link>
     </div>
   );
 }
-
--- app/components/ui/anchor.tsx
+++
@@ -1,5 +0,0 @@
-import { Link, LinkProps } from "@remix-run/react";
-
-export function Anchor(props: LinkProps) {
-    return <Link {...props} className={'hover:text-orange-500'}/>
-}
