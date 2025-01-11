79b46a62206334f3a8bfc432798400dabfd3f380
Maksim Prudnikau <60088883+MaksimPrudnikau@users.noreply.github.com>
2025-01-11 12:56:15 +0300

    init header markup

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -2,9 +2,23 @@ import HeaderBar from "~/components/header/HeaderBar";
 
 export default function Header() {
   return (
-    <div>
+    <>
       <HeaderBar />
-      This is Header
-    </div>
+
+      <header className={'flex gap-5'}>
+        <img src={'dodo.svg'} height={100} width={300} alt={'dodo'}/>
+          <div className={'flex flex-col gap-2'}>
+              <div className={'flex gap-1`'}>
+                  <h4>Доставка пиццы:</h4>
+                  <h4>Минск</h4>
+              </div>
+              <div className={'flex gap-1 items-center justify-start'}>
+                  <span>37 мин</span>
+                  <span className={'w-[4px] h-[5px] bg-black rounded-full'}></span>
+                  <span>4.76</span>
+              </div>
+          </div>
+      </header>
+    </>
   );
 }
