43045fef2d62635dfea8746b6fa1c6b80730634e
Maksim Prudnikau <60088883+MaksimPrudnikau@users.noreply.github.com>
2025-01-11 15:12:23 +0300

    add divider

--- app/components/header/HeaderBar.tsx
+++ app/components/header/HeaderBar.tsx
@@ -3,12 +3,14 @@ import { Link } from "@remix-run/react";
 
 export default function HeaderBar() {
   return (
-    <div className={"flex gap-5"}>
-      <Link id={"stream"} to={"/stream"}>
-        Прямой эфир
-      </Link>
-      <Link to={"/about"}>О нас</Link>
-      <Link to={"/contacts"}>Контакты</Link>
+    <div className={"py-4 border-b-2"}>
+      <div className={"flex gap-5"}>
+        <Link id={"stream"} to={"/stream"}>
+          Прямой эфир
+        </Link>
+        <Link to={"/about"}>О нас</Link>
+        <Link to={"/contacts"}>Контакты</Link>
+      </div>
     </div>
   );
 }
